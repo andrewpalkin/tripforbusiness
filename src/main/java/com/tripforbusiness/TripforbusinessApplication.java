3687a34d8c0e2edf7eb10904a2b1806e19b97ebd
Andrew Palkin <palkin.andrew@gmail.com>
2018-05-23 18:44:31 +0300

    Redux and Axios

--- src/main/java/com/tripforbusiness/TripforbusinessApplication.java
+++ src/main/java/com/tripforbusiness/TripforbusinessApplication.java
@@ -2,11 +2,25 @@ package com.tripforbusiness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages = {"com.tripforbusiness"})
 public class TripforbusinessApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TripforbusinessApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/login").allowedOrigins("*");
+            }
+        };
     }
 }
